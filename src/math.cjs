0da9e0ae697882b7d784601f9895e79d01557c64
midCenturyDude7 <mgriffes@gmail.com>
2023-03-28 11:24:37 -0700

    Working with Common JS and Node

---
+++ src/math.cjs
@@ -0,0 +1,15 @@
+exports.add = (a, b) => a + b;
+
+exports.subtract = (a, b) => {
+  return a - b;
+};
+
+exports.multiply = (a, b) => {
+  return a * b;
+};
+
+exports.divide = (a, b) => {
+  return a / b;
+};
+
+// module.exports = { add, subtract, multiply, divide };
---
+++ src/server.cjs
@@ -0,0 +1,23 @@
+console.log("Hello, World!");
+// console.log(global);
+
+// const os = require("os");
+// const path = require("path");
+const { add, subtract, multiply, divide } = require("./math.cjs");
+
+console.log(add(2, 3));
+console.log(subtract(10, 7));
+console.log(multiply(25, 22));
+console.log(divide(100, 50));
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
